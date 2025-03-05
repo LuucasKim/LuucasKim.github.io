143278e8d74f2a4f03671f73d7f2525db69e7a1f
LuucasKim <lucas0519@dgu.ac.kr>
2025-03-06 01:22:06 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ document.addEventListener("DOMContentLoaded", function () {
         }
         console.log(screenSize);
         const radius = screenSize / 2; // 반지름도 비율에 맞게 조정
-        const centerX = screenSize / 2;
-        const centerY = screenSize / 1.5;
+        const centerX = window.innerWidth / 2;
+        const centerY = window.innerHeight / 1.5;
         const itemSize = screenSize / 9; // 원 개별 크기 조절
 
         for (let i = 0; i < numItems; i++) {
