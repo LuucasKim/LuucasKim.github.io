838747594d55e1c51bdf46768e635f858a2ff0c5
LuucasKim <lucas0519@dgu.ac.kr>
2025-03-06 00:46:40 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
         container.innerHTML = ""; // 기존 요소 제거 후 다시 생성
 
         const numItems = 24;
-        const screenSize = Math.min(window.innerWidth * 0.8, window.innerHeight * 0.8); // 가로/세로 중 작은 값 기준
+        const screenSize = Math.min(window.innerWidth * 0.8, window.innerHeight * 0.8, 300); // 가로/세로 중 작은 값 기준
         const radius = screenSize / 2; // 반지름도 비율에 맞게 조정
         const centerX = screenSize / 2;
         const centerY = screenSize / 1.5;
